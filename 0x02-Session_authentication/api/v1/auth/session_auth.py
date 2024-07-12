91dc212e08c33cae5266114cf24b9b4d960cd5ce
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-07-12 17:06:10 +0100

    feat: Initial commit with basic session auth structure

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,1 +1,53 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Empty session"""
+
+from api.v1.auth.auth import Auth
+import uuid
+
+
+class SessionAuth(Auth):
+    """SessionAuth class.
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session.
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """User ID for session ID.
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Current user.
+        """
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+        user_id = self.user_id_for_session_id(session_cookie)
+        from models.user import User
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Destroy session.
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
