b02fb79297e3f0de572d75fe9e3302a79cc1d945
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-07-18 22:31:39 +0100

    feat: Define _hash_password method using bcrypt
    Defined `_hash_password` method to return a salted hash of the password.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Hash password """
+
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """ Return a salted, hashed password, which is a byte string """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check if password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+def _generate_uuid() -> str:
+    """ Generate a UUID """
+    return str(uuid.uuid4())
+
+
